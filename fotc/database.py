2acd94ebdcb7eaf630ff597c896040765496eac9
Stefan Puhlmann <hugopuhlmann@gmail.com>
2018-05-01 18:56:07 +0200

    Adding default values for missing database env vars.

--- fotc/database.py
+++ fotc/database.py
@@ -2,6 +2,8 @@
 
 import os
 import logging
+from typing import Text
+
 import sqlalchemy as sqla
 from sqlalchemy import Column, Integer, String, TIMESTAMP
 from sqlalchemy.ext.declarative import declarative_base
@@ -12,6 +14,14 @@ log = logging.getLogger("fotc")
 
 Base = declarative_base()
 
+def _get_env_default(name: Text, default_val: Text):
+    val = os.environ.get(name)
+    if val is None:
+        log.warning("Variable %s undefined, fall-backing to default value", name)
+        return default_val
+    return val
+
+
 def get_default_engine() -> sqla.engine.Engine:
     """
     Creates a database engine object from environment variables
@@ -20,17 +30,11 @@ def get_default_engine() -> sqla.engine.Engine:
         DATABASE_HOST: server hostname
         DATABASE_NAME: name of the database
         DATABASE_USER, DATABASE_PASS: credentials
-
-    If any of the variables is not defined, the function will raise KeyError
     """
-    try:
-        host = os.environ["DATABASE_HOST"]
-        name = os.environ["DATABASE_NAME"]
-        user = os.environ["DATABASE_USER"]
-        password = os.environ["DATABASE_PASS"]
-    except KeyError:
-        log.error("Missing one or more database environment variables")
-        raise
+    host = _get_env_default("DATABASE_HOST", "postgres")
+    name = _get_env_default("DATABASE_NAME", "fotc")
+    user = _get_env_default("DATABASE_USER", "fotc")
+    password = _get_env_default("DATABASE_PASS", "devdevdev")
     conn_str = f"postgresql://{user}:{password}@{host}/{name}"
     return sqla.create_engine(conn_str)
 
